236f66c0d58ea7e7718e09e8e10b3c82e3c63ed8
IntSur <1203394464@qq.com>
2024-01-04 23:49:48 +0800

    Renew ES6 all

---
+++ ES6/Lessons notes/babeledjs/module1.js
@@ -0,0 +1,10 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+//分别暴露
+var name = exports.name = 'module1';
+var function1 = exports.function1 = function function1() {
+    console.log('function1');
+};
---
+++ ES6/Lessons notes/babeledjs/module3.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+//默认暴露
+exports.default = {
+    name: 'module1',
+    function1: function function1() {
+        console.log('function1');
+    }
+};
